22a730052781a4137eb415b3763f5900f0592408
efotso <efotso@yahoo.com>
2013-02-10 21:19:49 -0800

    Update features/step_definitions/movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,17 +4,48 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  
+end
+
+Given /^I am the RottenPotatoes home page$/ do
+  visit('/movies')
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  ratings = rating_list.split
+  ratings.each do |r|
+    if uncheck
+      uncheck("ratings_#{r}")
+    else
+      check("ratings_#{r}")
+    end
+  end
+end
+
+When /^I submit the search forms$/ do
+  click_button "Refresh"
+end
+
+
+When /I sort the results by (.*)/ do |sort_order|
+  sort_id = sort_order.gsub(/\s/, '_')
+  click_link "#{sort_id}_header"
+end
+  
+Then /^I should see all of the movies$/ do
+  assert page.has_css?("table tbody tr", count: 10)
+end
+
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body =~ /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
